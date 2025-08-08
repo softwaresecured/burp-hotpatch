e836bdfcf4292d2522e5b20ddf1c859e441d396c
fi7z <dev@softwaresecured.com>
2025-08-07 23:20:18 -0400

    example scripts

---
+++ examples/utility/hostlist.py
@@ -0,0 +1,18 @@
+"""
+Author: Software Secured
+Script type: Utility
+Language: Python
+Description: Searches all responses in scope for URLS in quoted strings
+"""
+import re
+def extract_hosts( response_str ):
+	hosts = re.findall(r'["\']https?:\/\/([^\/"\':]+)', response_str, re.IGNORECASE|re.MULTILINE)
+	return list(set(hosts))
+
+def main( montoyaApi ):
+	discovered_hosts = []
+	for req_res in montoyaApi.proxy().history():
+		if not req_res.request().isInScope():
+			continue
+		discovered_hosts += extract_hosts(req_res.response().toString())
+	print("\n".join(list(set(discovered_hosts))))
