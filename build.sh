4751e3d3adbe132cff496bf028554f145c7a5eb2
bg <bg@inthebg>
2025-10-01 10:37:14 -0300

    got rid of shared memory exception, just return null/None in ScriptSharedMemory

---
+++ build.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH 
+
+./gradlew --info clean build
--- src/burp_hotpatch/scripts/ScriptSharedMemoryException.java
+++
@@ -1,6 +0,0 @@
-package burp_hotpatch.scripts;
-public class ScriptSharedMemoryException extends Exception {
-    public ScriptSharedMemoryException(String errorMessage) {
-        super(errorMessage);
-    }
-}
